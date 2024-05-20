6e749b1b09abd0e3927d5b9b2863cfbc8c49b29f
Cloud User <ec2-user@ip-172-31-53-208.ec2.internal>
2024-05-20 22:28:17 +0000

    setup map and data, working on API and presentation.

--- world-map/src/app/app.routes.ts
+++ world-map/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { WorldMapComponent } from './world-map/world-map.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: WorldMapComponent } 
+];
