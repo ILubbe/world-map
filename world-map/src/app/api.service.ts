7afa9dc2e525c459048ba2f8bda16f98db2d4ff3
Cloud User <ec2-user@ip-172-31-53-208.ec2.internal>
2024-05-22 19:56:32 +0000

    extended apiService, simplified world-map.component.ts

--- world-map/src/app/api.service.ts
+++ world-map/src/app/api.service.ts
@@ -9,10 +9,27 @@ import { Subject } from 'rxjs';
 export class ApiService {
 
   constructor(private http: HttpClient) { }
-
+  country = {};
   fetchCountryData(id: string) {
     let api_url = 'https://api.worldbank.org/v2/country/' + id + '?format=json';
     return this.http.get(api_url);
+  }
+
+  setCountryData(id: string) {
+    let subject = new Subject();
+
+    this.fetchCountryData(id).subscribe((data: any) => {
+      subject.next({
+        name: data[1][0].name,
+        capitalCity: data[1][0].capitalCity,
+        region: data[1][0].region.value,
+        incomeLevel: data[1][0].incomeLevel.value,
+        iso2Code: data[1][0].iso2Code,
+        latitude: data[1][0].latitude,
+        longitude: data[1][0].longitude
+      })
+    })
+    return subject.asObservable();
   }
 
 }
--- world-map/src/app/world-map/world-map.component.ts
+++ world-map/src/app/world-map/world-map.component.ts
@@ -15,19 +15,14 @@ export class WorldMapComponent {
 
   constructor (private apiService: ApiService) {}
 
-  setCountryData(event: any) {
-    this.apiService.fetchCountryData(event.target.getAttribute('id')).subscribe((data: any) => {
+  printCountryData(event: any) {
+    this.apiService.setCountryData(event.target.id).subscribe((data: any) => {
       this.country = {
-        name: data[1][0].name,
-        capitalCity: data[1][0].capitalCity,
-        region: data[1][0].region.value,
-        incomeLevel: data[1][0].incomeLevel.value,
-        iso2Code: data[1][0].iso2Code,
-        latitude: data[1][0].latitude,
-        longitude: data[1][0].longitude
+        ...data,
       }
     })
-  }
+  } 
+
   clearCountryData() {
     this.country = {
       name: '',
